8e9c8b353cb7785a6644934c4b743c2c48a92762
xyder <xydr2311@gmail.com>
2017-02-10 07:53:04 +0200

    Switched to using a setup in columns_to_dict test case.

--- tests/test_core/test_db_access_control/test_query_builder/test_query_builder_class.py
+++ tests/test_core/test_db_access_control/test_query_builder/test_query_builder_class.py
@@ -4,43 +4,33 @@ from core.db_access_control import QueryBuilder
 
 
 class ColumnsToDictTestCase(unittest.TestCase):
-    def test_happy_flow(self):
-        # build test resources
-        test_columns = [
+
+    def setUp(self):
+        self.test_columns = [
             ('test_attr_1', None),  # simulating a Column object from SQLA
             ('test_attr_2', None),
             ('test_attr_3', None),
         ]
 
-        test_object = type('', (object,), {
+        self.test_object = type('', (object,), {
             'test_attr_1': 'test_val_1',
             'test_attr_2': 'test_val_2',
             'test_attr_3': ''  # empty string to check filtering
         })()
 
-        # run test
-        result = QueryBuilder.columns_to_dict(test_object, test_columns)
+    def test_happy_flow(self):
+        """ Test most common successfull flow. """
+
+        result = QueryBuilder.columns_to_dict(self.test_object, self.test_columns)
         self.assertDictEqual(result, {
             'test_attr_1': 'test_val_1',
             'test_attr_2': 'test_val_2'
         })
 
     def test_with_no_filtering(self):
-        # build test resources
-        test_columns = [
-            ('test_attr_1', None),  # simulating a Column object from SQLA
-            ('test_attr_2', None),
-            ('test_attr_3', None),
-        ]
+        """ Test successfull flow with filtering disabled. """
 
-        test_object = type('', (object,), {
-            'test_attr_1': 'test_val_1',
-            'test_attr_2': 'test_val_2',
-            'test_attr_3': ''
-        })()
-
-        # run test
-        result = QueryBuilder.columns_to_dict(test_object, test_columns, filtered=False)
+        result = QueryBuilder.columns_to_dict(self.test_object, self.test_columns, filtered=False)
         self.assertDictEqual(result, {
             'test_attr_1': 'test_val_1',
             'test_attr_2': 'test_val_2',
@@ -48,22 +38,14 @@ class ColumnsToDictTestCase(unittest.TestCase):
         })
 
     def test_non_existent_column(self):
-        # build test resources
-        test_columns = [
-            ('test_attr_1', None),  # simulating a Column object from SQLA
-            ('test_attr_2', None),
-            ('test_attr_3', None),
-            ('test_attr_4', None),  # non-existent column to test exception
-        ]
+        """ Test exception when a non-existent column is requested. """
 
-        test_object = type('', (object,), {
-            'test_attr_1': 'test_val_1',
-            'test_attr_2': 'test_val_2',
-            'test_attr_3': ''
-        })()
+        # customize test resources
+        test_columns = self.test_columns.copy()
+        test_columns.append(('test_attr_4', None))
 
         # run test
-        self.assertRaises(AttributeError, QueryBuilder.columns_to_dict, test_object, test_columns)
+        self.assertRaises(AttributeError, QueryBuilder.columns_to_dict, self.test_object, test_columns)
 
 
 class BuildClauseTestCase(unittest.TestCase):
